1d94bcd25ff144de9cbd9e806316d079db5d73d5
robsonsilv4 <robsonsilv410@gmail.com>
2020-01-27 11:23:56 -0300

    Correção das queries

--- src/config/server.js
+++ src/config/server.js
@@ -3,10 +3,12 @@ const port = 3003;
 const bodyParser = require('body-parser');
 const express = require('express');
 const server = express();
+const queryParser = require('express-query-int');
 const allowCors = require('./cors');
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+server.use(queryParser());
 server.use(allowCors);
 
 server.listen(port, function() {
